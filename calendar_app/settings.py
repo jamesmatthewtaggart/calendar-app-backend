72f227fdb89e2d468c4c19ccca906ba34e41ca2d
James Taggart <jamesmatthewtaggart@gmail.com>
2021-01-14 15:15:24 +0000

    Environment variables.

--- calendar_app/settings.py
+++ calendar_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "=754$6yc97wno3hj!w_99=hd#-wkt$8tl7s@25-f_f^^bypb^1"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
